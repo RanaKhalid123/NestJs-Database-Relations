6582f077a1725042101a480b270f5f0774e7add0
RanaKhalid123 <khalidrasool768@yahoo.com>
2021-09-01 20:04:07 +0500

    nestjs database relations using TypeOrm

---
+++ ormconfig.ts
@@ -0,0 +1,17 @@
+// import { SqliteConnectionOptions } from "typeorm/driver/sqlite/SqliteConnectionOptions";
+
+// const config: SqliteConnectionOptions = {
+//     type: 'sqlite',
+//     database: 'database',
+//     entities: ['dist/src/**/*.entity.js'],
+//     synchronize: true,
+//     logging: true,
+//     // migrations: [
+//     //     'dist/src/database/migrations/*.js'
+//     // ],
+//     // cli: {
+//     //     migrationsDir: 'src/database/migrations'
+//     // }
+// }
+
+// export default config;
---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// import config  from '../ormconfig'
+import { Employee } from './employee.entity';
+import { ContactInfo } from './contact-info-entity';
+import { Meeting } from './meeting.entity';
+import { Task } from './task.entity';  
+
+@Module({
+  imports: [TypeOrmModule.forRoot({
+    type: 'sqlite',
+    database: 'database',
+    entities: ['dist/**/*.entity.js'],
+    synchronize: true,
+    logging: true,
+  }),
+  TypeOrmModule.forFeature([Employee,ContactInfo,Meeting,Task])], 
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
